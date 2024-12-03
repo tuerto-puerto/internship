2807a15ec007ef3c07bdc554e2bc8c5abb86faea
Turat <torotura@icloud.com>
2024-12-03 12:19:41 +0600

    1

--- Entity.go
+++ Entity.go
@@ -3,18 +3,22 @@ package main
 import "fmt"
 
 type User struct {
-	id       int
-	name     string
-	email    string
-	password string
+	id        int
+	firstName string
+	lastName  string
+	age       string
+	email     string
+	password  string
 }
 
 func main() {
 	user := User{
-		id:       1,
-		name:     "пальто",
-		email:    "пальто.пацан@gmail.com",
-		password: "123",
+		id:        1,
+		firstName: "пальто",
+		lastName:  "пацан",
+		age:       "18 лет",
+		email:     "пальто.пацан@gmail.com",
+		password:  "123	",
 	}
 
 	fmt.Println("User", user)
